9ff72adf01a383e61af3cd640969b09ef15e7d0f
ednezer <ednezerrezende>
2020-07-09 19:05:14 -0300

    Inserindo PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent {
   showLevel1 = null;
   paginas: any;
 
+  showBtn = false;
+  deferredPrompt: any;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -29,6 +32,43 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+    });
+
+    this.checarPWA();
+  }
+
+  checarPWA() {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later on the button event.
+      this.deferredPrompt = e;
+
+      // Update UI by showing a button to notify the user they can add to home screen
+      this.showBtn = true;
+    });
+
+    window.addEventListener('appinstalled', (event) => {
+      console.log('installed');
+    });
+
+    if (window.matchMedia('(display-mode: standalone)').matches) {
+      console.log('display-mode is standalone');
+    }
+  }
+
+  instalarPWA(e) {
+    // hide our user interface that shows our button
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the prompt');
+      } else {
+        console.log('User dismissed the prompt');
+      }
+      this.deferredPrompt = null;
     });
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { CoreModule } from './core/core/core.module';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpConfigInterceptor } from './interceptors/httpConfig.interceptor';
 import { OrderModule } from 'ngx-order-pipe';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule,  AppRoutingModule, OrderModule ],
+  imports: [CoreModule,  AppRoutingModule, OrderModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }) ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
